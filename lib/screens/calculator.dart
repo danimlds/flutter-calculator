e1524c9bd3903246372c3363cb86aa4329e013ad
Daniel Moura <daniel.mlds@gmail.com>
2021-03-23 15:13:47 -0300

    calculator design modification

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,15 +1,29 @@
+import 'package:calculadora/models/memory.dart';
 import 'package:flutter/material.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 
-class Calculator extends StatelessWidget {
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.apllyCommand(command);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
         children: <Widget>[
-          Display('Daniel Moura'),
-          Keyboard(),
+          Display(memory.value),
+          Keyboard(_onPressed),
         ],
       ),
     );
